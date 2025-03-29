cc1098718c4b5bd7ccfb6afd54f34ab587ed3462
evfedorovyh <jhon2002@mail.ru>
2025-03-29 20:19:08 +0300

    Модель данных - Post. [#285211]

---
+++ src/main/java/model/Post.java
@@ -0,0 +1,36 @@
+package model;
+
+import java.util.Objects;
+
+public class Post {
+    long id;
+    String  title;
+    String link;
+    String description;
+    long time;
+
+    @Override
+    public boolean equals(Object o) {
+        if (o == null || getClass() != o.getClass()) {
+            return false;
+        }
+        Post post = (Post) o;
+        return Objects.equals(link, post.link);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hashCode(link);
+    }
+
+    @Override
+    public String toString() {
+        return "Post{"
+                + "id=" + id
+                + ", title='" + title + '\''
+                + ", link='" + link + '\''
+                + ", description='" + description + '\''
+                + ", time=" + time
+                + '}';
+    }
+}
