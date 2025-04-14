5496d6aee3033ac2a74feb1c7af911710187ec95
evfedorovyh <jhon2002@mail.ru>
2025-04-14 05:46:59 +0300

    2. Парсинг HTML страницы. [#260358]

--- src/main/java/ru/job4j/Main.java
+++ src/main/java/ru/job4j/Main.java
@@ -3,12 +3,14 @@ package ru.job4j;
 import org.apache.log4j.Logger;
 import ru.job4j.grabber.model.Post;
 import ru.job4j.grabber.service.Config;
+import ru.job4j.grabber.service.HabrCareerParse;
 import ru.job4j.grabber.service.SchedulerManager;
 import ru.job4j.grabber.service.SuperJobGrab;
 import ru.job4j.grabber.stores.JdbcStore;
 
 import java.sql.DriverManager;
 import java.sql.SQLException;
+import java.util.List;
 
 public class Main {
     private static final Logger LOGGER = Logger.getLogger(Main.class);
@@ -21,10 +23,16 @@ public class Main {
                 config.get("db.username"),
                 config.get("db.password"))) {
             var store = new JdbcStore(connection);
-            var post = new Post();
+            HabrCareerParse parser = new HabrCareerParse();
+            List<Post> listParse = parser.fetch();
+            for (Post post : listParse) {
+                store.save(post);
+            }
+/*            var post = new Post();
             post.setTitle("Super Java Job");
             post.setTime(System.currentTimeMillis());
-            store.save(post);
+            store.save(post);*/
+
             var scheduler = new SchedulerManager();
             scheduler.init();
             scheduler.load(
