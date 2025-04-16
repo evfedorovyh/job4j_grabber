295f141989a13e4dce06f8ce71a60995362c713e
evfedorovyh <jhon2002@mail.ru>
2025-04-16 22:21:01 +0300

    1.1. Job c параметрами [#260360 #539792]

--- src/main/java/ru/job4j/Main.java
+++ src/main/java/ru/job4j/Main.java
@@ -26,6 +26,7 @@ public class Main {
                     SuperJobGrab.class,
                     store
             );
+            Thread.sleep(100000);
         } catch (SQLException e) {
             LOGGER.error("When creating a connection", e);
         }
--- src/main/java/ru/job4j/grabber/service/SuperJobGrab.java
+++ src/main/java/ru/job4j/grabber/service/SuperJobGrab.java
@@ -5,21 +5,21 @@ import org.quartz.Job;
 import org.quartz.JobExecutionContext;
 import org.quartz.JobExecutionException;
 import ru.job4j.grabber.model.Post;
-import ru.job4j.grabber.stores.JdbcStore;
 import ru.job4j.grabber.stores.Store;
 import ru.job4j.grabber.utils.HabrCareerDateTimeParser;
-
-import java.sql.DriverManager;
-import java.sql.SQLException;
 import java.util.List;
 
 public class SuperJobGrab implements Job {
-    private static final Logger LOGGER = Logger.getLogger(SuperJobGrab.class);
 
     @Override
     public void execute(JobExecutionContext context) throws JobExecutionException {
-/*        var store = (Store) context.getJobDetail().getJobDataMap().get("store");*/
-        var config = new Config();
+        var store = (Store) context.getJobDetail().getJobDataMap().get("store");
+        HabrCareerParse parser = new HabrCareerParse(new HabrCareerDateTimeParser());
+        List<Post> listParse = parser.list("https://career.habr.com");
+        for (Post post : listParse) {
+            store.save(post);
+        }
+/*        var config = new Config();
         config.load("application.properties");
         try (var connection = DriverManager.getConnection(config.get("db.url"),
                 config.get("db.username"),
@@ -32,6 +32,6 @@ public class SuperJobGrab implements Job {
             }
         } catch (SQLException e) {
             LOGGER.error("When creating a connection", e);
-        }
+        }*/
     }
 }
